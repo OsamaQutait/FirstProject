5ae843e2521456a2502d0a6bc9a37f5a5477e088
Osama Qutait <oaqutait@gmail.com>
2022-12-01 21:28:59 +0200

    generate_random_number

--- child.cpp
+++ child.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int generate_random_number(int lower, int upper)
+{
+    int s, numb;
+    FILE *f;
+    f = fopen("/dev/urandom", "rb");
+    fread(&s, sizeof(int), 1, f);
+    fclose(f);
+    srand(s);
+    int num = (rand() % (upper - lower + 1)) + lower;
+    return num;
+
+}
 int main(int argc, char *argv[]) {
     cout << "Hello, World!, child" << endl;
     return 0;
