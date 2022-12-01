cc76da1793e53e59211c1af91c55fedae95b0a5a
Osama Qutait <oaqutait@gmail.com>
2022-12-01 20:08:35 +0200

    Initial commit

---
+++ parent.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+int main(int argc, char *argv[]) {
+    cout << "Hello, World!, parent" << endl;
+    return 0;
+}
