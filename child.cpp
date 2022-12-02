018b3992861143860f367a421fa10c82614cd7e2
FayezBackleh <fayezBackleh@axsos.me>
2022-12-02 19:07:20 +0200

    last step

--- child.cpp
+++ child.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
 
 
 int generate_Speed(int lower, int upper) {
@@ -24,19 +25,29 @@ int generate_Speed(int lower, int upper) {
 
 int main(int argc, char *argv[]) {
     cout << "Hello, World!, child" << endl;
-    string type = argv[1];
+
+    const char *type = argv[1];
+    cout << type << endl;
+
 
     kill(getpid(), SIGSTOP);
     int speed = generate_Speed(80, 100);
+    cout << speed << endl;
+
     usleep(speed * 20000);
     //REd sigusr1
 
-    if (type == "red") {
-        kill(getppid(), SIGUSR1);
+    if ( strcmp(type, "red") == 0) {
+        cout << "iam in the red if";
+
+             kill(getppid(), SIGUSR1);
     }
-    if (type == "green") {
+    if (strcmp(type, "green") == 0) {
+        cout << "iam in the green if";
+
         kill(getppid(), SIGUSR2);
         //REd sigusr2
     }
+    pause();
     return 0;
 }
