a20cf7774b0c42c520e5091d5371a59ad23ad69a
FayezBackleh <fayezBackleh@axsos.me>
2022-12-02 14:24:21 +0200

    fayez signal catcher

--- child.cpp
+++ child.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
+#include <unistd.h>
+#include <signal.h>
 
-int generate_random_number(int lower, int upper)
+
+
+int generate_Speed(int lower, int upper)
 {
     int s, numb;
     FILE *f;
@@ -12,7 +16,17 @@ int generate_random_number(int lower, int upper)
     int num = (rand() % (upper - lower + 1)) + lower;
     return num;
 }
+
+void signal_catcher1(int sig){
+
+
+}
+
+
 int main(int argc, char *argv[]) {
     cout << "Hello, World!, child" << endl;
+    int speed = generate_Speed(80,100);
+    usleep(speed*10000);
+    signal(SIGUSR1, signal_catcher1);
     return 0;
 }
